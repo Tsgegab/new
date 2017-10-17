6fc8451e55cc11cab82c022df907afc36aa79415
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2017-10-17 14:46:04 -0400

    intial rectangle program

---
+++ Rectagle.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert;
+import java.util.Optional;
+	public class Rectangle extends Application {
+		@Override
+		public void start(Stage primarystage){
+			TextInpuDialog dialog = new TextInputDialog();
+			dilaog.setHeaderText("Input the width and height of the Rectangle");
+			Optional<String> input = dialog.showAndWait();
+			String [] side = input.get().split(" ");
+			double width = Double.parseDouble(side[0]);
+			double height = Double.parseDouble(side[1]);
+			double perimeter = 2*width + 2*height;
+			double area = width*height;
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setContentText("The perimeter of the Rectangle is : " + perimeter + "/n the area of the Rectangle" + area);
+				alert.showAndWait();
+				}
+}
+
